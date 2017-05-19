f62568a3121453def002f6203bb1cc0093819ea8
KIMO\Maxym <maxymkarpinsky@gmail.com>
2017-05-19 22:07:32 +0300

    Update Lab_2 - new diagram, add diagram sketch, new task description, update report

---
+++ Lab_2/Queries/SQLQuery_Formula.sql
@@ -0,0 +1,10 @@
+﻿USE Lab_2_Variant_8_Students_Maxym_Karpinsky;
+GO
+
+SELECT DATEPART(year, [Students].admission_date) - DATEPART(year, [Students].birth_date)
+FROM [Students];
+
+SELECT CONCAT('Директор', ' ', [sc].[name], ' ', CONCAT([scd].fist_name, ' ', [scd].middle_name, ' ', [scd].last_name)) 
+FROM [School] AS sc
+JOIN [School_Directors] AS scd
+ON sc.director_id = scd.id;

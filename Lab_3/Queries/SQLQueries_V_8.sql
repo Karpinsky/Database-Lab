847c364f95743f2033ac7c8812623ac2c88e22f5
KIMO\Maxym <maxymkarpinsky@gmail.com>
2017-05-22 15:34:14 +0300

    Add tasks 3. 5. queried

--- Lab_3/Queries/SQLQueries_V_8.sql
+++ Lab_3/Queries/SQLQueries_V_8.sql
@@ -103,13 +103,21 @@ BEGIN
 END
 GO
 
--- 1) БД «Кораблі».Перерахувати назви головних кораблів (із таблиці Ships). Вивести: name, class. Вихідні дані впорядкувати за зростанням за стовпцем name.
+/*
+	1) БД «Кораблі».
+	Перерахувати назви головних кораблів (із таблиці Ships). 
+	Вивести: name, class. 
+	Вихідні дані впорядкувати за зростанням за стовпцем name.
+*/
 SELECT [Ships].[name] AS ship_name, [Ships].[class] AS ship_class
 FROM [Ships]
 WHERE [Ships].name = [Ships].class
 ORDER BY ship_name;
 
--- 2) БД «Аеропорт».Вивести прізвища пасажирів (друге слово в стовпці name), що починаються на літеру 'С'.
+/*
+	2) БД «Аеропорт».
+	Вивести прізвища пасажирів (друге слово в стовпці name), що починаються на літеру 'С'.
+*/
 -- CASE 1 - Not giving the correct data because of middle or tripple names in name field. Look at CASE 2 for more correct data.
 WITH SplitNameData AS
 (
@@ -135,5 +143,90 @@ CROSS APPLY (SELECT * FROM [SO_CUT_STRING_TO]([Passenger].[name], ' ', 1)) AS [p
 WHERE [passenger_full_last_name].[string_part] LIKE 'C%'
 GO
 
+/*
+	3) БД «Комп. фірма». 
+	Знайдіть пари моделей ноутбуків, що мають однакові об’єми жорстких дисків та RAM (таблиця Laptop). 
+	У результаті кожна пара виводиться лише один раз. 
+	Порядок виведення: модель із більшим номером, модель із меншим номером, об’єм диску та RAM.
+*/
+SELECT DISTINCT ((CAST([LP_one].[model] AS INT) + CAST([LP_two].[model] AS INT)) + ABS(CAST([LP_one].[model] AS INT) - CAST([LP_two].[model] AS INT))) / 2 AS [model_one], 
+	((CAST([LP_one].[model] AS INT) + CAST([LP_two].[model] AS INT)) - ABS(CAST([LP_one].[model] AS INT) - CAST([LP_two].[model] AS INT))) / 2 AS [model_two],
+	[LP_one].hd,
+	[LP_one].ram
+FROM [Laptop] AS LP_one
+JOIN [Laptop] AS LP_two
+ON [LP_one].ram = [LP_two].ram AND [LP_one].hd = [LP_two].hd AND [LP_one].code != [LP_two].code;
 
+/*
+	4) БД «Комп. фірма». 
+	Знайдіть виробників, що випускають ПК, але не ноутбуки (використати ключове слово ANY). 
+	Виведіть: maker.
+*/
+
+
+/*
+	5) БД «Комп. фірма». 
+	Знайдіть виробників принтерів, що випускають ПК із найменшим об’ємом RAM. 
+	Виведіть: maker.
+*/
+DECLARE @PCMinRam INT;
+SELECT @PCMinRam = MIN([PC].ram) FROM [PC];
+
+DECLARE @PrinterMakers TABLE (maker VARCHAR(1) UNIQUE);
+INSERT INTO @PrinterMakers
+SELECT [Product].[maker]
+FROM [Product]
+JOIN [Printer]
+ON [Product].[model] = [Printer].[model]
+GROUP BY [Product].[maker]
+
+SELECT DISTINCT [Product].[maker]
+FROM [Product]
+JOIN [PC]
+ON [Product].[model] = [PC].[model] AND [PC].[ram] = @PCMinRam AND [Product].[maker] IN (SELECT * FROM @PrinterMakers);
+
+/*
+	6) БД «Аеропорт».
+	Вивести дані для таблиці Trip з об’єднаними значеннями двох стовпців: town_from та town_to, 
+	з додатковими коментарями типу: 'from Rostov to Paris'.
+*/
+
+
+/*
+	7) БД «Комп. фірма». 
+	Знайти моделі та ціни ПК, вартість яких перевищує мінімальну вартість ноутбуків. 
+	Вивести: model, price.
+*/
+
+
+/*
+	8) БД «Комп. фірма». 
+	Знайдіть виробників, які б випускали ПК із мінімальною швидкістю не менше 500 МГц. 
+	Вивести: maker, мінімальна швидкість. 
+	(Підказка: використовувати підзапити в якості обчислювальних стовпців)
+*/
+
+
+/*
+	9) БД «Фірма прий. вторсировини». 
+	Визначіть лідера за сумою виплат у змаганні між кожною парою пунктів 
+	з однаковими номерами із двох різних таблиць – Outcome та Outcome_o – на кожний день, 
+	коли здійснювався прийом вторинної сировини хоча б на одному з них. 
+	Вивести: Номер пункту, дата, текст: 
+	– 'once a day', якщо сума виплат є більшою у фірми зі звітністю один раз на день; 
+	– 'more than once a day', якщо – у фірми зі звітністю декілька разів на день; 
+	– 'both', якщо сума виплат є однаковою.  
+	(Підказка: для з’єднання таблиць використовувати повне зовнішнє з’єднання, 
+	а для перевірки умов оператор CASE; 
+	для пунктів, що не мають у певні дні видачі грошей – необхідно обробляти NULL - 
+	значення за допомогою перевірки умови IS [NOT] NULL)
+*/
+
+
+/*
+	10) БД «Комп. фірма».
+	Для кожної моделі продукції з усієї БД виведіть її середню ціну. 
+	Вивести: type, model, середня ціна. 
+	(Підказка: використовувати оператор UNION)
+*/
 
